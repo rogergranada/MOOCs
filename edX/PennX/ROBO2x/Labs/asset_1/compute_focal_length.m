41637816e683cd10bebf891d783fe0b775dbcc57
Roger Granada <roger.leitzke@gmail.com>
2018-02-16 18:48:53 -0200

    added code Matlab Dolly zoom

---
+++ edX/PennX/ROBO2x/Labs/asset_1/compute_focal_length.m
@@ -0,0 +1,14 @@
+function [ f ] = compute_focal_length( d_ref, f_ref, pos )
+% compute camera focal length using given camera position
+%
+% Input:
+% - d_ref: 1 by 1 double, distance of the object whose size remains constant
+% - f_ref: 1 by 1 double, previous camera focal length
+% - pos: 1 by n, each element represent camera center position on the z axis.
+% Output:
+% - f: 1 by n, camera focal length
+
+    pos = d_ref - pos;
+    f = (f_ref.*pos)./d_ref;
+end
+
