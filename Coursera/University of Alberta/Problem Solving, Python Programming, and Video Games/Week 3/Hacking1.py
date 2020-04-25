e9cc18a2fc2b3d7d4fb89928b5244e83d818b277
Roger Granada <roger.leitzke@gmail.com>
2020-04-25 04:52:19 -0300

    Hacking version 1

---
+++ Coursera/University of Alberta/Problem Solving, Python Programming, and Video Games/Week 3/Hacking1.py
@@ -0,0 +1,41 @@
+# Code for Hacking1.py
+
+# Hacking Version 1
+# This is a text-based password guessing game that displays a
+# list of potential computer passwords. The player is allowed
+# 1 attempt to guess the password. The game indicates that the
+# player failed to guess the password correctly.
+
+# display header
+print('DEBUG MODE')
+print('1 ATTEMPT(S) LEFT')
+print('')
+
+# display password list
+print('PROVIDE')
+print('SETTING')
+print('CANTINA')
+print('CUTTING')
+print('HUNTERS')
+print('SURVIVE')
+print('HEARING')
+print('HUNTING')
+print('REALIZE')
+print('NOTHING')
+print('OVERLAP')
+print('FINDING')
+print('PUTTING')
+print('')
+
+# prompt for guess
+input('Enter password >')
+
+# end game
+# display failure outcome
+print('LOGIN FAILURE - TERMINAL LOCKED')
+print('')
+print('PLEASE CONTACT AN ADMINISTRATOR')
+print('')
+
+# prompt for end
+input('PRESS ENTER TO EXIT')
